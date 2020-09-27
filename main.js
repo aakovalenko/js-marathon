0a7c73b9f0935a49e58293701fff8d8f56abb833
Andrii <kaa89pl@gmail.com>
2020-09-27 11:12:20 +0300

    zar hometask2

--- main.js
+++ main.js
@@ -1,36 +1,59 @@
 const firstRow = 'мама мыла раму';
 const secondRow = 'собака друг человека';
 
-function getRow(firstRow, secondRow) {
-	let str1 = firstRow.length;
-	let str2 = secondRow.length;
-	let countStr1 = 0;
-	let countStr2 = 0;
+// function getRow(firstRow, secondRow) {
+// 	let str1 = firstRow.length;
+// 	let str2 = secondRow.length;
+// 	let countStr1 = 0;
+// 	let countStr2 = 0;
 
-	for(let i = 0; i < str1; i++) {
-		if (firstRow.charAt(1) == firstRow[i]) {
-			countStr1 += 1;
-		}
+// 	for(let i = 0; i < str1; i++) {
+// 		if (firstRow.charAt(1) == firstRow[i]) {
+// 			countStr1 += 1;
+// 		}
 
-	}
+// 	}
 
-	for(let i = 0; i < str2; i++) {
-		if (secondRow.charAt(3) == secondRow[i]) {
-			countStr2 += 1;
-		}
+// 	for(let i = 0; i < str2; i++) {
+// 		if (secondRow.charAt(3) == secondRow[i]) {
+// 			countStr2 += 1;
+// 		}
 
-	}
+// 	}
 
 
-	if(countStr1 > countStr2) {
-		return(firstRow);
-	} else if (countStr1 < countStr2) {
-		return(secondRow);
-	} else if (countStr1 == countStr2) {
-		return(firstRow + ' ' + secondRow);
-	} else {
-		return ('no str');
-	}
+// 	if(countStr1 > countStr2) {
+// 		return(firstRow);
+// 	} else if (countStr1 < countStr2) {
+// 		return(secondRow);
+// 	} else if (countStr1 == countStr2) {
+// 		return(firstRow + ' ' + secondRow);
+// 	} else {
+// 		return ('no str');
+// 	}
+// }
+
+// console.log(getRow(firstRow, secondRow));
+
+function getRow2(firstRow, secondRow) {
+let char = 'а';
+let firstRes = countedChar(firstRow, char);
+let secondtRes = countedChar(secondRow, char);
+
+
+return firstRes > secondtRes ? firstRow : secondRow;
+console.log(firstRes);
+console.log(secondtRes);
 }
 
-console.log(getRow(firstRow, secondRow));
+console.log(getRow2(firstRow,secondRow));
+
+function countedChar(row, char) {
+	let counted = 0;
+	for (let i = 0; i < row.length; i++) {
+if (row.charAt(i) === char) {
+	counted++;
+	}
+}
+return counted;
+}
